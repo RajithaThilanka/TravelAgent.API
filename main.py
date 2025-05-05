4313f8000ca8f96495924e7e99a3ec575fbf7407
rajithat <rajithat@itx360.com>
2025-05-06 01:19:55 +0530

    Add Init

--- main.py
+++ main.py
@@ -283,4 +283,4 @@ async def start_chat():
 # Start the app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
